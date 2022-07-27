0243bee4cd07307db415e9f00d8c6cf1bc275717
unknown <marioposadamiranda@gmail.com>
2022-07-26 23:18:12 -0300

    fix: onMouseEnter

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -22,27 +22,27 @@ const MenuItems = ({ items, depthLevel }) => {
     };
   }, [dropdown]);
 
-  const onMouseEnter = () => {
-    window.innerWidth > 960 && setDropdown(true);
-  };
+//   const onMouseEnter = () => {
+//     window.innerWidth > 960 && setDropdown(true);
+//   };
 
-  const onMouseLeave = () => {
-    window.innerWidth > 960 && setDropdown(false);
-  };
+//   const onMouseLeave = () => {
+//     window.innerWidth > 960 && setDropdown(false);
+//   };
 
   return (
     <li
       className="menu-items"
       ref={ref}
-      onMouseEnter={onMouseEnter}
+    //   onMouseEnter={onMouseEnter}
     //   onMouseLeave={onMouseLeave}
     >
       {items.submenu ? (
         <>
           <button
             type="button"
-            aria-haspopup="menu"
-            aria-expanded={dropdown ? "true" : "false"}
+            // aria-haspopup="menu"
+            // aria-expanded={dropdown ? "true" : "false"}
             onClick={() => setDropdown((prev) => !prev)}
           >
             {items.title}{" "}
