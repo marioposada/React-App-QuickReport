590d0622da418d9f5a8d077dddb259a212fec202
unknown <marioposadamiranda@gmail.com>
2022-08-10 20:41:58 -0300

    Set inputs

--- src/components/FormReport.js
+++ src/components/FormReport.js
@@ -1,17 +1,38 @@
 import React from "react";
-import "./formReport.css"
+import "./formReport.css";
 
 const FormReport = () => {
   return (
-    <div>
+    <div className="wrapperForm">
       <h2 className="title">Formulario</h2>
       <form className="form">
-        <input type="text" name="name" placeholder="Escribe tu nombre" required />
-        <input type="text" name="email" placeholder="Escribe tu nombre" required/>
-        <input type="date" />
-        <input type="text" />
-      <textarea name="comments"  cols="50" rows="5"></textarea>
+        <input type="number" name="informe" placeholder="N° Informe" required />
+        <input type="date" name="fecha" placeholder="Fecha" required />
+        <input type="text" name="proveedor" placeholder="Proveedor" required />
+        <input type="number" name="oe" placeholder="OE N°" required />
+        <input type="number" name="remito" placeholder="N° Remito" required />
+        <input type="text" name="cid" placeholder="CID" required />
+        <input type="text" name="cliente" placeholder="Cliente" required />
+        <input
+          type="text"
+          name="plano"
+          placeholder="Numero de plano"
+          required
+        />
+        <input
+          type="text"
+          name="ubicacion"
+          placeholder="Ubicacion del producto"
+          required
+        />
+          <input
+          type="file"
+          name="file"
+          placeholder="Imagen"
+          required
+        />
 
+        <textarea name="comments" cols="50" rows="5" required></textarea>
       </form>
     </div>
   );
